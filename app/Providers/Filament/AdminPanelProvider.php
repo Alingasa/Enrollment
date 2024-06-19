51271bf071707607175a611c5c4a37a700ebc8da
Alingasa <jamespagiwayanalingasa@gmail.com>
2024-06-20 02:37:13 +0800

    commited

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->brandName('HighSchool-Enrollment')
             ->login()
             ->colors([
                 'primary' => Color::Sky,
