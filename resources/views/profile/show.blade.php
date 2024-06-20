62667e051ebf6c32cbae4567f8edd09ecbf8cb15
jandel lopez <jandellopez1997.gmail.com>
2024-06-20 21:46:28 +0800

    qr code bugs

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -145,9 +145,9 @@
         <div class="photo">
             {{-- <img src="{{ $record->profile_image ?? 'http://try.test/default_images/me.jpg' }}" alt="profile"> --}}
             @if ($record->profile_image)
-            <img src='{{"profile_image/".$record->profile_image}}'; alt="profile"/>
+            <img src='{{"http://highschoolenrollment.webactivities.online/storage/".$record->profile_image}}'; alt="profile"/>
             @else
-            <img src="{{'profile_iamge/'.$record->profile_image}}" alt="profile">
+            <img src="{{'http://highschoolenrollment.webactivities.online/default_images/me.jpg'}}" alt="profile">
             @endif
 
         </div>
