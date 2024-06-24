c87289399577cbdaebabd2a32103a5c93a8fd281
Alingasa <jamespagiwayanalingasa@gmail.com>
2024-06-25 05:24:51 +0800

    commited

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Api\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EnrollmentController;
-
+use App\Http\Controllers\PDFController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -14,3 +14,8 @@ Route::get('/findschoolid/{school_id}',[EnrollmentController::class,'findschooli
 Route::get('/profile/{hash}', [ProfileController::class, 'show'])->name('profile.show');
 Route::get('/teacher/{hash}', [ProfileController::class, 'teacher'])->name('teacher');
 Route::put('/studentsUpdate/{school_id}', [EnrollmentController::class, 'updateSchool']);
+
+
+
+//Print PDF
+Route::get('download', [PDFController::class, 'downloadpdf'])->name('download.tes');
