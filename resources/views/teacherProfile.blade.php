d75523993c1e01b661976555720ab61d9bffdaa4
Alingasa <jamespagiwayanalingasa@gmail.com>
2024-06-26 04:45:29 +0800

    commited

--- resources/views/teacherProfile.blade.php
+++ resources/views/teacherProfile.blade.php
@@ -26,7 +26,7 @@
             margin-bottom: 10px;
         }
         .gradesheet-title {
-            font-size: 24px;
+            font-size: 18px;
             font-weight: bold;
             color: red;
             margin-bottom: 10px;
@@ -40,7 +40,10 @@
     </style>
 </head>
 <body>
+    {{-- <div style="text-align: center;">
+        {!! QrCode::size(200)->generate(route('profile.show', Crypt::encryptString($teacher->id))) !!}
 
+    </div> --}}
 <div class="container gradesheet-container">
     <div class="gradesheet-header">
         {{-- <img src="logo.png" alt="MLG College of Learning, Inc."> --}}
@@ -48,26 +51,36 @@
         {{-- <p>Brgy. Atabay, Hilongos, Leyte</p> --}}
     </div>
     {{-- <h5 class="text-right">GRADESHEET</h5> --}}
+{{-- @include('filament.resources.student-resource.pages.teacher') --}}
+{{-- <div style="flex: 1; text-align: center;">
+    <div style="display: inline-block; padding: 10px; background: #fff; border-radius: 10px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+        {!! QrCode::size(200)->generate(route('profile.show', Crypt::encryptString($teacher->id))) !!}
 
+    </div> --}}
     <div class="personal-info">
+
+{{-- @dd(QrCode::size(200)->generate(route('profile.show', Crypt::encryptString($teacher->id)))) --}}
         @if($teacher->profile_image)
         <img style="height: 140px; width: 140px;" src="{{public_path('storage/'.$teacher->profile_image)}}" alt="">
         @else
         <img src="{{public_path('default_images/me.jpg')}}" alt="">
         @endif
 
-        <p>Name: {{$teacher->full_name}}</p>
-        <p>Contact:{{$teacher->contact_number}}1</p>
-        <p>Gender: {{$teacher->gender}}</p>
+        <p> <b>Name:</b> {{$teacher->full_name}}</p>
+        <p><b>Contact:</b>{{$teacher->contact_number}}1</p>
+        <p><b>Gender:</b> {{$teacher->gender}}</p>
         {{-- <p>Civil Status: {{$teacher->civil_status}}</p> --}}
-        <p>Religion: {{$teacher->zip_code}}</p>
-        <p>Address: {{$teacher->barangay. ' '. $teacher->municipality.','.' '.$teacher->province}}</p>
-        <p>Birth Date: {{$teacher->birthdate}}</p>
+        <p><b>Religion:</b> {{$teacher->zip_code}}</p>
+        <p><b>Address:</b> {{$teacher->barangay. ' '. $teacher->municipality.','.' '.$teacher->province}}</p>
+        <p><b>Birth Date:</b> {{$teacher->birthdate}}</p>
     </div>
 
-    {{-- <div class="gradesheet-title text-center">
-        Bachelor of Science in Information Technology
-    </div> --}}
+    <div class="gradesheet-title">
+       Incase of Emergency
+    </div>
+    <p> <b>Guardian/Parent:</b> {{$teacher->guardian_name}}</p>
+    <p><b>Contact:</b>{{$teacher->incaseof_emergency}}</p>
+
     {{-- <p class="text-center">Third Year (Second Semester, AY 2023-2024)</p> --}}
 
     <div class="emergency-contact">
@@ -104,13 +117,13 @@
                 <td>{{$loop->iteration}}</td>
                 <td>{{$subjects->subject_code}}</td>
                 <td>{{$subjects->subject_title}}</td>
-                <td>{{$subjects->section->name}}</td>
+                <td>{{$sections->name}}</td>
                 <td><?php foreach ($subjects['day'] as $day): ?>
                     <?php echo $day.','; ?>
                  <?php endforeach; ?>/{{$subjects->time_start.'-'.$subjects->time_end}}</td>
                  <td>{{$subjects->subject_type}}</td>
                  <td>{{$subjects->units}}</td>
-                 <td>{{$subjects->room}}</td>
+                 <td>{{$rooms->room}}</td>
             </tr>
             @endforeach
 
