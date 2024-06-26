f728c608e49bfbc5a52d9a4670fd648256e255fd
Alingasa <jamespagiwayanalingasa@gmail.com>
2024-06-26 22:07:52 +0800

    commited

--- app/Filament/Teacher/Resources/TeacherResource/Pages/Dashboard.php
+++ app/Filament/Teacher/Resources/TeacherResource/Pages/Dashboard.php
@@ -2,12 +2,13 @@
 
 namespace App\Filament\Teacher\Resources\TeacherResource\Pages;
 
+use App\Filament\Resources\TeacherResource as ResourcesTeacherResource;
 use App\Filament\Teacher\Resources\TeacherResource;
 use Filament\Resources\Pages\Page;
 
 class Dashboard extends Page
 {
-    protected static string $resource = TeacherResource::class;
+    protected static string $resource = ResourcesTeacherResource::class;
 
     protected static string $view = 'filament.teacher.resources.teacher-resource.pages.dashboard';
 }
