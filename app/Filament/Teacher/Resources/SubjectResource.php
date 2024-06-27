4f3ec68e072cdbbab9b6f60db0b8a0c2d3e6d2ea
Alingasa <jamespagiwayanalingasa@gmail.com>
2024-06-27 22:37:05 +0800

    committed

--- app/Filament/Teacher/Resources/SubjectResource.php
+++ app/Filament/Teacher/Resources/SubjectResource.php
@@ -132,9 +132,9 @@ class SubjectResource extends Resource
                 // Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                // Tables\Actions\BulkActionGroup::make([
+                //     Tables\Actions\DeleteBulkAction::make(),
+                // ]),
             ]);
     }
 
