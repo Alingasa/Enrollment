b594217634dc554ebe1340c78be2429c13482660
Alingasa <jamespagiwayanalingasa@gmail.com>
2024-06-27 23:56:31 +0800

    commited

--- resources/views/studentprofile.blade.php
+++ resources/views/studentprofile.blade.php
@@ -5,19 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Student Profile</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
     <style>
         body {
             font-family: Arial, sans-serif;
             line-height: 1.6;
             background-color: #f8f9fa;
         }
-        .container {
+        .gradesheet-container {
             max-width: 800px;
             margin: 20px auto;
             padding: 20px;
+            /* border: 1px solid #ddd; */
+            border-radius: 5px;
+            /* background-color: #fff; */
         }
         .gradesheet-header {
             text-align: center;
@@ -27,13 +27,18 @@
             width: 100px;
             margin-bottom: 10px;
         }
+        .gradesheet-title {
+            font-size: 24px;
+            font-weight: bold;
+            color: red;
+            margin-bottom: 10px;
+        }
         .personal-info, .emergency-contact {
             margin-bottom: 20px;
         }
         .gradesheet-table {
             width: 100%;
             border-collapse: collapse;
-            margin-top: 20px;
         }
         .gradesheet-table th, .gradesheet-table td {
             border: 1px solid #ddd;
@@ -45,30 +50,34 @@
             background-color: #f2f2f2;
             font-weight: bold;
         }
+        .table-header {
+            text-align: center;
+            margin-bottom: 10px;
+        }
     </style>
 </head>
 <body>
-    <div class="container">
+    <div class="container gradesheet-container">
         <div class="gradesheet-header">
             <h2>Personal Information</h2>
         </div>
 
         <div class="personal-info">
             <div>
-                @if($data->profile_image)
-                    <img style="height: 140px; width: 140px;" src="{{public_path('storage/'.$data->profile_image)}}" alt="">
-                @else
-                    <img src="{{public_path('default_images/me.jpg')}}" alt="">
-                @endif
+            @if($data->profile_image)
+            <img style="height: 140px; width: 140px;" src="{{public_path('storage/'.$data->profile_image)}}" alt="">
+            @else
+            <img src="{{public_path('default_images/me.jpg')}}" alt="">
+            @endif
             </div>
             <div>
-                <p>Name: {{ $data->full_name }}</p>
-                <p>Contact: {{ $data->contact_number }}</p>
-                <p>Gender: {{ $data->gender }}</p>
-                <p>Civil Status: {{ $data->civil_status->getLabel() }}</p>
-                <p>Religion: {{ $data->religion }}</p>
-                <p>Address: {{ $data->barangay . ' ' . $data->municipality . ' ' . $data->province }}</p>
-                <p>School ID: {{ $data->school_id ?: 'Not provided' }}</p>
+            <p>Name: {{ $data->full_name }}</p>
+            <p>Contact: {{ $data->contact_number }}</p>
+            <p>Gender: {{ $data->gender }}</p>
+            <p>Civil Status: {{ $data->civil_status->getLabel() }}</p>
+            <p>Religion: {{ $data->religion }}</p>
+            <p>Address: {{ $data->barangay . ' ' . $data->municipality . ' ' . $data->province }}</p>
+            <p>School ID: {{ $data->school_id ?: 'Not provided' }}</p>
             </div>
         </div>
 
@@ -78,7 +87,7 @@
             <p>Number: {{ $data->incaseof_emergency }}</p>
         </div>
 
-        <div class="gradesheet-header">
+        <div class="table-header">
             <h2>Subjects</h2>
         </div>
 
@@ -99,24 +108,23 @@
             <tbody>
                 @foreach ($subjects as $index => $sub)
                 <tr>
-                    <td>{{ $loop->iteration }}</td>
+                    <th scope="row">{{ $loop->iteration }}</th>
                     <td>{{ $sub->subject_code ?: 'Not provided' }}</td>
                     <td>{{ $sub->subject_title ?: 'Not provided' }}</td>
-                    <td>{{ $sub->section->name ?: 'Not provided' }}</td>
+                    <td>{{ $section->section->name ?: 'Not provided' }}</td>
                     <td>
-                        @if (isset($teachers[$index]))
-                            {{ $teachers[$index]->teacher->full_name }}
-                        @else
-                            Not provided
-                        @endif
+                        <?php if (isset($teachers[$index])): ?>
+                        <?php echo $teachers[$index]->teacher->full_name; ?>
+                        <?php endif; ?>
                     </td>
-                    <td>
-                        {{ implode(', ', $sub['day']) }} / {{ $sub->time_start . '-' . $sub->time_end }}
-                    </td>
-                    <td>{{ $sub->subject_type ?: 'Not provided' }}</td>
-                    <td>{{ $sub->units ?: 'Not provided' }}</td>
-                    <td>{{ $sub->room ?: 'Not provided' }}</td>
-                </tr>
+                    {{-- @dd($sub['day']); --}}
+                    <td><?php foreach ($sub['day'] as $day): ?>
+                        <?php echo $day.','; ?>
+                        <?php endforeach; ?>/{{$sub->time_start.'-'.$sub->time_end}}</td>
+                        <td>{{ $sub->subject_type ?: 'Not provided' }}</td>
+                        <td>{{ $sub->units ?: 'Not provided' }}</td>
+                        <td>{{ $sub->room ?: 'Not provided' }}</td>
+                    </tr>
                 @endforeach
             </tbody>
         </table>
