2c119a05b8f83a4b381d514fa8475143a0aa5b40
Alingasa <jamespagiwayanalingasa@gmail.com>
2024-06-27 20:10:49 +0800

    committed

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -158,7 +158,7 @@
             <p><span>Age:</span> {{$record->age}}</p>
             <p><span>GUARDIAN/PARENT:</span> {{$record->guardian_name}}</p>
             <div class="signature">
-                <p class="signature-label">Student Signature</p>
+                <p class="signature-label">Signature</p>
 
             </div>
         </div>
