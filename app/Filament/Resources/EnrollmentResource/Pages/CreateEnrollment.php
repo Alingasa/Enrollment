28ecb4e462534fe6b3692e62143485d9b6698d3c
Alingasa <jamespagiwayanalingasa@gmail.com>
2024-06-27 21:33:36 +0800

    committed

--- app/Filament/Resources/EnrollmentResource/Pages/CreateEnrollment.php
+++ app/Filament/Resources/EnrollmentResource/Pages/CreateEnrollment.php
@@ -2,11 +2,14 @@
 
 namespace App\Filament\Resources\EnrollmentResource\Pages;
 
-use App\Filament\Resources\EnrollmentResource;
 use Filament\Actions;
+use Filament\Facades\Filament;
+use Illuminate\Database\Eloquent\Model;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\EnrollmentResource;
 
 class CreateEnrollment extends CreateRecord
 {
     protected static string $resource = EnrollmentResource::class;
+
 }
--- resources/views/filament/resources/student-resource/pages/teacher.blade.php
+++ resources/views/filament/resources/student-resource/pages/teacher.blade.php
@@ -1,4 +1,5 @@
 {{-- <x-filament::page> --}}
+    @if($getRecord())
     <div style="display: flex; align-items: center; padding: 20px; border: 1px solid #ddd; border-radius: 10px; background: linear-gradient(135deg, #f9f9f9, #ececec); box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
         {{-- <div style="flex: 1; text-align: center;">
             <img src="{{ $record->profile_image ? url($record->profile_image) : url('default_images/me.jpg') }}" alt="Profile Image" style="border-radius: 50%; width: 150px; height: 150px; object-fit: cover; border: 3px solid #fff; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
@@ -11,9 +12,10 @@
         {{-- @dd($getRecord()) --}}
         <div style="flex: 1; text-align: center;">
             <div style="display: inline-block; padding: 10px; background: #fff; border-radius: 10px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
-                {!! QrCode::size(200)->generate(route('teacher', Crypt::encryptString($getRecord()))) !!}
+                {!! QrCode::size(200)->generate(route('teacher', Crypt::encryptString($getRecord()->id))) !!}
 
             </div>
         </div>
     </div>
+    @endif
 {{-- </x-filament::page> --}}
